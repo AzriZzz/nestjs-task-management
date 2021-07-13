345c166351223d7dc80b7f6c827de163bc488db0
Muhammad Azri <muhammad.azri@avanade.com>
2021-07-13 23:31:32 +0800

    [Feature]: Path up data

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,6 +1,14 @@
-import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
 import { CreateTaskDTO } from './dto/create-task.dto';
-import { Task } from './task.model';
+import { Task, TaskStatus } from './task.model';
 import { TasksService } from './tasks.service';
 
 @Controller('tasks')
@@ -21,6 +29,14 @@ export class TasksController {
   @Delete('/:id')
   deleteTaskById(@Param('id') id: string): void {
     return this.tasksService.deleteTask(id);
+  }
+
+  @Patch('/:id/status')
+  updateTaskById(
+    @Param('id') id: string,
+    @Body('status') status: TaskStatus,
+  ): Task {
+    return this.tasksService.updateTaskById(id, status);
   }
 
   @Post()
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -19,6 +19,18 @@ export class TasksService {
     this.tasks = this.tasks.filter((task) => task.id !== id);
   }
 
+  updateTaskById(id: string, status: TaskStatus): Task {
+    // const data = this.getTaskById(id);
+    // data.status = status;
+
+    // return data;
+
+    const index = this.tasks.findIndex((task) => task.id === id);
+    this.tasks[index].status = status;
+
+    return this.tasks[index];
+  }
+
   createTasks(createTaskDTO: CreateTaskDTO): Task {
     const { title, description } = createTaskDTO;
 
