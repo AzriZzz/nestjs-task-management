ef85aa44ae3cfd6f7d02f1be38f033db98afccfd
Muhammad Azri <lenovorosak@gmail.com>
2021-07-23 23:30:24 +0800

    feature: connect with postgres

---
+++ src/auth/users.repository.ts
@@ -0,0 +1,19 @@
+/* eslint-disable prettier/prettier */
+import { EntityRepository, Repository } from 'typeorm';
+import { AuthCredentialsDto } from './dto/auth-credentials.dto';
+import { User } from './user.entity';
+
+@EntityRepository(User)
+export class UserRepository extends Repository<User> {
+  async createUser(authCredentialsDto: AuthCredentialsDto): Promise<void> {
+    const { username, password } = authCredentialsDto;
+
+    const user = this.create({
+      username,
+      password
+    })
+
+    await this.save(user);
+    // return user;
+  }
+}
